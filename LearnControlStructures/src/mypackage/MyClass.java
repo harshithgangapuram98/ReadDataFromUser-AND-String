f588e2de232d3a02bee074679d285a660fa00a51
Harshith <harshithganga98@gmail.com>
2022-09-13 10:46:56 +0530

    Loops

---
+++ LearnControlStructures/src/mypackage/MyClass.java
@@ -0,0 +1,49 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		//variable declaration
+		/*int i;
+		
+		for(i=1;i<=5;i++) {
+			System.out.println("Hello");
+		System.out.println("Hello");
+		}*/
+		
+		/*
+		 int i;
+		for(i=1;i<=5;i++)
+			System.out.println(i);
+		 */
+		
+		/*
+		int i;
+		for(i=1;i<=5;System.out.println(i))
+			i++;
+		*/
+		
+		
+		/*int i;
+		for(i=1;i<=5;i--)
+			System.out.println("hello");*/
+		/*
+		int n,i;
+		
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter any number : ");
+		n=obj.nextInt();
+		
+		
+		
+		for(i=1;i<=n;i++) {
+			if(n%i==0)
+				System.out.println(i);
+		}*/
+			
+	}
+
+}
